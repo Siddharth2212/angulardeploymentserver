d759eb36ca5ee3fc29648a94c59321264de3a238
Siddharth Sogani <siddharthsogani22@gmail.com>
2018-03-28 09:38:41 +0530

    aeg

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use('/', index);
 app.use('/users', users);
 */
 
-app.get('/*', function (req, res) {
+/*app.get('/!*', function (req, res) {
     res.sendFile('/dist/index.html');
-});
+});*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
